9f018a8a10d133ec94342021d1624eacb02d861c
Fabricio Orrala <fzzio007@gmail.com>
2024-01-31 18:22:14 -0500

    Using environment vars

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const apiKey = 'e756c1eb867cada2fafddf1890b0a680';
+const apiKey: string | undefined = import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY;
+
+if (!apiKey) {
+  throw new Error('API key not defined in .env file');
+}
 
 export const getWeatherData = async (city: string) => {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
